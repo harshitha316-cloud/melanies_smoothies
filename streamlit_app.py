983adedf5f9426cf57687e5ce8673d554a528078
harshitha316-cloud <harshitha316@gmail.com>
2024-11-04 11:34:01 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,4 +65,5 @@ if time_to_insert:
    #st.success(Messg ,icon="✅")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+#st.text(fruityvice_response.json())
+fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
